17aa05ba0d216e80cc0c18216d9e3c2e437648e4
AsfafathimaN <98514212+AsfafathimaN@users.noreply.github.com>
2022-11-12 17:20:56 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,7 @@
+
+
+
+
+filename=input("input the filename:")
+f_extns=filename.split(".")
+print("the extension of the file is :"+repr(f_extns[-1]))
---
+++ radius.py
@@ -0,0 +1,8 @@
+from math import pi
+
+r=float(input("enter radius of a circle"))
+
+
+print("Area of the circle is,"+str(pi*r**2))
+
+        
